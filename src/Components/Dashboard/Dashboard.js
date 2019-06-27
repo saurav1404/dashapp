d442d9a5271d8ad961457daf0a98d2a9428a74c6
saurav1404 <sanspoly@gmail.com>
2019-06-27 19:14:21 +0530

    added text append

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -11,18 +11,26 @@ export class Dashboard extends Component {
     super(props);
     this.state = {
       response: false,
-      endpoint: "http://8e9f2298.ngrok.io"
+      textList: [],
+      endpoint: "http://8e9f2298.ngrok.io",
+      textEndPoint: "http://2c53a468.ngrok.io"
     };
   }
 
   componentDidMount() {
-    const { endpoint } = this.state;
-    const socket = socketIOClient(endpoint);
-    socket.on("update", data => this.setState({ response: data }));
+    const { textEndPoint } = this.state;
+    let { textList } = this.state;
+    // const socket = socketIOClient(endpoint);
+    // socket.on("update", data => this.setState({ response: data }));
+    const socket = socketIOClient(textEndPoint);
+    socket.on("update", data => {
+      textList.push(data);
+      this.setState({textList})
+    });
   }
 
   render() {
-    const { response } = this.state;
+    const { response, textList } = this.state;
     return (
       <div className="container-fluid app-body dashboard">
         <Row>
@@ -40,15 +48,11 @@ export class Dashboard extends Component {
           <Col className="column" xs={24} sm={24} md={12} lg={12} xl={12}>
             <Card title="Card title">
               <div className="card-content card-list-content">
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
+                {
+                  textList.map((text, key) => (
+                    <p key = {key}>{text}</p>
+                  ))
+                }
               </div>
             </Card>
           </Col>
